dcb3c3324fc2cb7ec6553b2e892cd88958e991cd
Kayra <kayrabtuncer@gmail.com>
2021-03-16 14:48:20 +0300

    Registration

---
+++ src/users/user.model.ts
@@ -0,0 +1,12 @@
+import * as mongoose from 'mongoose'
+import User from './user.interface'
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+})
+
+const userModel = mongoose.model<User & mongoose.Document>('User', userSchema)
+
+export default userModel
